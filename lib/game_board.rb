326527b4444e003b80f49043caa07c87cfece15c
hykare <78385964+hykare@users.noreply.github.com>
2022-08-09 17:52:51 +0200

    put new tokens into the game board

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,3 +1,11 @@
 class GameBoard
+  attr_reader :state
 
+  def initialize(state = Array.new(7) { [] })
+    @state = state
+  end
+
+  def update(value, column)
+    state[column].push(value)
+  end
 end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -1,7 +1,31 @@
 require_relative '../lib/game_board'
 
-describe 'GameBoard' do
+describe GameBoard do
   describe '#draw'
-  describe '#update'
+
+  describe '#update' do
+    context 'when the board is empty' do
+      subject(:game_board) { described_class.new }
+
+      it 'puts in a token in an empty column' do
+        value = :player1
+        column = 0
+        expect { game_board.update(value, column) }.to change { game_board.state[column] }.to([value])
+      end
+    end
+
+    context 'when there are already tokens in the board' do
+      let(:state) { [[:player1, :player2], [], [:player1, :player1, :player2], [:player2], [], [], []] }
+      subject(:game_board) { described_class.new(state) }
+
+      it 'adds the token at the top of a column' do
+        value = :player1
+        column = 2
+        game_board.update(value, column)
+        expect(game_board.state[column].last).to be value
+      end
+    end
+  end
+
   describe '#game_over?'
 end
